f5676794605bf8f2c0d1972a71fc4e77c2a330e1
ashuza <ashuzamh@gmail.com>
2026-02-08 11:26:12 +0100

    chore: Remove useless files

--- src/apps/settings/production.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-from apps.config import ProductionConfig
-
-
-class Config(ProductionConfig):
-    # Azure Container Apps injects env vars via KeyVault & Bicep
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{config('DBUSER')}:{config('DBPASS')}@{config('DBHOST')}/{config('DBNAME')}"
-    SECRET_KEY = config("FLASKSECRET", default="change-this")
-    DEBUG = False
